7d15014e4c3a3569e0daab0276ef64c412d2b3f8
Nick Sergeant <nick@nicksergeant.com>
2014-05-29 11:37:56 -0400

    Push button, get GIF.

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 'use strict';
 
+var exec = require('child_process').exec;
 var five = require('johnny-five');
 var Spark = require('spark-io');
+var request = require('request');
+
 var board = new five.Board({
   io: new Spark({
     token: process.env.SPARK_TOKEN,
@@ -9,7 +12,35 @@ var board = new five.Board({
   })
 });
 
+console.log('Using SPARK_TOKEN ' + process.env.SPARK_TOKEN + '.');
+console.log('Using SPARK_DEVICE_ID ' + process.env.SPARK_DEVICE_ID + '.');
+
 board.on('ready', function() {
+
+  console.log('Spark ready...');
+
+  var button = new five.Button('D5');
+  var gifs;
   var led = new five.Led('D7');
-  led.blink();
+
+  board.repl.inject({
+    button: button
+  });
+
+  request('http://gifs.is/api/items', function (err, response, body) {
+    gifs = JSON.parse(body.replace(')]}\',\n', ''));
+  });
+
+  button.on('down', function() {
+    led.on();
+    if (gifs.length) {
+      var randGif = gifs[Math.floor(Math.random() * gifs.length) + 0].url;
+      exec('open ' + randGif);
+    }
+  });
+
+  button.on('up', function() {
+    led.off();
+  });
+
 });
